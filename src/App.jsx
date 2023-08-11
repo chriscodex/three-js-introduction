b733e2ab89bd5c93e208ad6ccd6291c61cbcc0c3
Christian <c.espinoza.code@gmail.com>
2023-08-11 17:27:10 -0500

    feat: hello cube from docs created

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,51 @@
 import { useEffect } from 'react';
 import './App.css';
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 function App() {
   useEffect(() => {
     const scene = new THREE.Scene();
-
     const camera = new THREE.PerspectiveCamera(
       50,
       window.innerWidth / window.innerHeight,
-      1,
+      0.1,
       1000
     );
-    camera.position.z = 96;
 
-    const canvas = document.getElementById('myThreeJsCanvas');
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
-    });
-
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
+    camera.position.z = 5;
 
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial()
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(boxMesh);
-
-    const animate = () => {
-      boxMesh.rotation.x += 0.01;
-      boxMesh.rotation.y += 0.01;
+    function animate() {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
       renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
-    };
+    }
 
-    animate();
+    if ( WebGL.isWebGLAvailable() ) {
+
+      // Initiate function or other initializations here
+      animate();
+    
+    } else {
+    
+      const warning = WebGL.getWebGLErrorMessage();
+      document.getElementById( 'container' ).appendChild( warning );
+    
+    }
   }, []);
 
   return (
     <div>
-      <canvas id="myThreeJsCanvas"></canvas>
     </div>
   );
 }
