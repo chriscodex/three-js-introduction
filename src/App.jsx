6b19761a823d9b17ad0629b09a55fed28dfdd640
Christian <c.espinoza.code@gmail.com>
2023-08-11 08:27:32 -0500

    feat: example from docs done

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,71 @@
 import { useEffect } from 'react';
 import './App.css';
 import * as THREE from 'three';
-import WebGL from 'three/addons/capabilities/WebGL.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 function App() {
   useEffect(() => {
     const scene = new THREE.Scene();
+
     const camera = new THREE.PerspectiveCamera(
-      50,
+      100,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
+    camera.position.z = 96;
 
-    const renderer = new THREE.WebGLRenderer();
+    const canvas = document.getElementById('myThreeJsCanvas');
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+    });
+
+    // Orbit controlls
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    // Add FPS stats.
+    const stats = Stats();
+    document.body.appendChild(stats.dom);
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    ambientLight.castShadow = true;
+    scene.add(ambientLight);
 
-    camera.position.z = 5;
+    const spotLight = new THREE.SpotLight(0xffffff, 1);
+    spotLight.castShadow = true;
+    spotLight.position.set(0, 64, 32);
+    scene.add(spotLight);
 
-    function animate() {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('/smoliv/scene.gltf', (gltfScene) => {
+      // transform & scale
+      // gltfScene.scene.rotation.y = Math.PI / 8;
+      // gltfScene.scene.position.y = 3;
+      // gltfScene.scene.scale.set(10,10,10);
+
+      scene.add(gltfScene.scene);
+    })
+
+    const animate = () => {
+      stats.update();
+      controls.update();
+
       renderer.render(scene, camera);
-    }
+      window.requestAnimationFrame(animate);
+    };
 
-    if ( WebGL.isWebGLAvailable() ) {
-
-      // Initiate function or other initializations here
-      animate();
-    
-    } else {
-    
-      const warning = WebGL.getWebGLErrorMessage();
-      document.getElementById( 'container' ).appendChild( warning );
-    
-    }
+    animate();
   }, []);
 
   return (
     <div>
+      <canvas id="myThreeJsCanvas"></canvas>
     </div>
   );
 }
