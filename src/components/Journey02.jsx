849e05855ad42a5f68c81e9af0249f1cb6e68ce4
Christian <c.espinoza.code@gmail.com>
2023-08-11 20:17:41 -0500

    feat: meterial for the cube created

--- src/components/Journey02.jsx
+++ src/components/Journey02.jsx
@@ -11,6 +11,7 @@ function Journey02() {
 
     // Red Cube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   }, []);
   return (
     <div>
