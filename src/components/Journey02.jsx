dacc2272efa2db03eb63ca718c95157935e6a3a9
Christian <c.espinoza.code@gmail.com>
2023-08-12 20:18:55 -0500

    feat: mesh added to the scene

--- src/components/Journey02.jsx
+++ src/components/Journey02.jsx
@@ -13,6 +13,7 @@ function Journey02() {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
   }, []);
   return (
     <div>
