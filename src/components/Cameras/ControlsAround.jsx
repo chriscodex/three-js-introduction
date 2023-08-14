b1210f97ab03c8c5e7b30d452d700b62162f34e5
Christian <c.espinoza.code@gmail.com>
2023-08-14 17:33:01 -0500

    feat: Controls manually created around the cube, example created

---
+++ src/components/Cameras/ControlsAround.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+function ControlsAround() {
+  // Canvas
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    // Scene - Like the scenario
+    const scene = new THREE.Scene();
+
+    // Red Cube
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    // Edges
+    const edges = new THREE.EdgesGeometry(geometry);
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+    const edgesMesh = new THREE.LineSegments(edges, lineMaterial);
+    mesh.add(edgesMesh)
+    scene.add(mesh);
+
+    // Sizes
+    const sizes = {
+      width: 800,
+      height: 600,
+    };
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      1,
+      100
+    );
+    camera.position.z = 3;
+    camera.lookAt(mesh.position);
+    scene.add(camera);
+
+    /* Cursor Movement for Camera */
+    const cursor = {
+      x: 0,
+      y: 0,
+    };
+    window.addEventListener('mousemove', (event) => {
+      cursor.x = event.clientX / sizes.width - 0.5;
+      cursor.y = -(event.clientY / sizes.height - 0.5);
+    });
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+
+    /* Animations */
+    // Instance the clock
+    const clock = new THREE.Clock();
+
+    const tick = () => {
+      // Clock
+      const elapsedTime = clock.getElapsedTime();
+
+      // mesh.rotation.y = (elapsedTime * Math.PI) / 2;
+
+      camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+      camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+      camera.position.y = cursor.y * 10
+      camera.lookAt(mesh.position);
+
+      // Rendering every tick
+      renderer.render(scene, camera);
+
+      // Method that call the next frame
+      window.requestAnimationFrame(tick);
+    };
+    tick();
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
+}
+
+export { ControlsAround }
