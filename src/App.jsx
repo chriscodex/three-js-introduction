8b8fc24da955eb074f34cbef6fdaea2581967781
Christian <c.espinoza.code@gmail.com>
2023-08-14 19:18:10 -0500

    feat: gsap dependency installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import { Move } from './components/Move';
-import { Groups } from './components/Groups';
-import { Animation } from './components/Animation';
+import { Move } from './components/Movements/Move';
+import { Groups } from './components/Movements/Groups';
+import { Animation } from './components/Animations/Animation';
+import { GsapAnimation } from './components/Animations/GsapAnimation';
+import { Perspective } from './components/Cameras/Perspective';
+import { Orthographic } from './components/Cameras/Orthographic';
+import { Controls } from './components/Cameras/Controls';
+import { ControlsAround } from './components/Cameras/ControlsAround';
+import { Orbit } from './components/Cameras/Orbit';
 import './App.css';
 
 function App() {
@@ -9,9 +15,13 @@ function App() {
       {/* <h2>Movement</h2>
       <Move />
       <h2>Groups</h2>
-      <Groups /> */}
+      <Groups />
       <h3>Animation</h3>
-      <Animation />
+      <Animation /> */}
+      {/* <h1>Gsap Animation</h1>
+      <GsapAnimation /> */}
+      <h1>Controls Around</h1>
+      <Orbit />
     </div>
   );
 }
