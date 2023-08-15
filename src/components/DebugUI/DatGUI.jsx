b622b00cd2b187dd397587bcb5db8c3351419d2b
Christian <c.espinoza.code@gmail.com>
2023-08-15 17:02:00 -0500

    feat: datgui refactorized

--- src/components/DebugUI/DatGUI.jsx
+++ src/components/DebugUI/DatGUI.jsx
@@ -2,36 +2,76 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { gsap } from 'gsap';
+
+const gui = new dat.GUI({closed: true, width: 400});
+
+const parameters = {
+  color: 0xff0000,
+  spin: () => {
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+  },
+};
+
+gui.addColor(parameters, 'color').onChange(() => {
+  material.color.set(parameters.color);
+});
+
+gui.add(parameters, 'spin');
+
+// Scene - Like the scenario
+const scene = new THREE.Scene();
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  100
+);
+camera.position.z = 3;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+/* Debug UI */
+gui.add(mesh.position, 'x', -3, 3, 0.01);
+gui.add(mesh.position, 'z', -3, 3, 0.01);
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation');
+
+gui.add(mesh, 'visible');
+
+gui.add(material, 'wireframe');
 
 function DatGUI() {
   // Canvas
   const canvasRef = useRef(null);
 
-  useEffect(() => {    
-    // Scene - Like the scenario
-    const scene = new THREE.Scene();
+  useEffect(() => {
+    // Controls
+    const controls = new OrbitControls(camera, canvasRef.current);
+    controls.enableDamping = true;
+    // controls.target.y = 1;
+    // controls.target.x = 1;
+    // controls.update();
 
-    // Red Cube
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const mesh = new THREE.Mesh(geometry, material);
-
-    // Edges
-    const edges = new THREE.EdgesGeometry(geometry);
-    const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
-    const edgesMesh = new THREE.LineSegments(edges, lineMaterial);
-    mesh.add(edgesMesh);
-    scene.add(mesh);
-
-    /* Debug UI */
-    const gui = new dat.GUI();
-    gui.add(mesh.position, 'y', -3, 3, 0.01)
-
-    // Sizes
-    const sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(window.devicePixelRatio);
 
     /* Resize */
     window.addEventListener('resize', () => {
@@ -47,64 +87,6 @@ function DatGUI() {
       renderer.setSize(sizes.width, sizes.height);
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     });
-
-    // Fullscreen Mode
-    window.addEventListener('dblclick', () => {
-      const canvas = canvasRef.current;
-
-      if (canvas) {
-        if (canvas.requestFullscreen) {
-          if (document.fullscreenElement) {
-            document.exitFullscreen();
-          } else {
-            canvas.requestFullscreen();
-          }
-        } else if (canvas.mozRequestFullScreen) {
-          if (document.mozFullScreenElement) {
-            document.mozCancelFullScreen();
-          } else {
-            canvas.mozRequestFullScreen();
-          }
-        } else if (canvas.webkitRequestFullscreen) {
-          if (document.webkitFullscreenElement) {
-            document.webkitExitFullscreen();
-          } else {
-            canvas.webkitRequestFullscreen();
-          }
-        } else if (canvas.msRequestFullscreen) {
-          if (document.msFullscreenElement) {
-            document.msExitFullscreen();
-          } else {
-            canvas.msRequestFullscreen();
-          }
-        }
-      }
-    });
-
-    // Camera
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      sizes.width / sizes.height,
-      1,
-      100
-    );
-    camera.position.z = 3;
-    camera.lookAt(mesh.position);
-    scene.add(camera);
-
-    // Controls
-    const controls = new OrbitControls(camera, canvasRef.current);
-    controls.enableDamping = true;
-    // controls.target.y = 1;
-    // controls.target.x = 1;
-    // controls.update();
-
-    // Renderer
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvasRef.current,
-    });
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(window.devicePixelRatio);
 
     /* Animations */
     const tick = () => {
@@ -127,4 +109,4 @@ function DatGUI() {
   );
 }
 
-export { DatGUI }
+export { DatGUI };
