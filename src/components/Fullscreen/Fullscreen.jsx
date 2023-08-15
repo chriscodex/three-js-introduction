1ee2729adf3d9fdf2b87241db8ff82afddaeba27
Christian <c.espinoza.code@gmail.com>
2023-08-15 09:47:16 -0500

    feat: initial structure for fullscreen component created

---
+++ src/components/Fullscreen/Fullscreen.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import './Fullscreen.css';
+
+function Fullscreen() {
+  // Canvas
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    // Scene - Like the scenario
+    const scene = new THREE.Scene();
+
+    // Red Cube
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    // Edges
+    const edges = new THREE.EdgesGeometry(geometry);
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+    const edgesMesh = new THREE.LineSegments(edges, lineMaterial);
+    mesh.add(edgesMesh);
+    scene.add(mesh);
+
+    // Sizes
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      1,
+      100
+    );
+    camera.position.z = 3;
+    camera.lookAt(mesh.position);
+    scene.add(camera);
+
+    // Controls
+    const controls = new OrbitControls(camera, canvasRef.current);
+    controls.enableDamping = true;
+    // controls.target.y = 1;
+    // controls.target.x = 1;
+    // controls.update();
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    /* Animations */
+    const tick = () => {
+      // Control update for damping
+      controls.update();
+
+      // Rendering every tick
+      renderer.render(scene, camera);
+
+      // Method that call the next frame
+      window.requestAnimationFrame(tick);
+    };
+    tick();
+  }, []);
+
+  return (
+    <div>
+      <canvas className="webgl" ref={canvasRef}></canvas>
+    </div>
+  );
+}
+
+export { Fullscreen };
