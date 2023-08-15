8252ce04b3ceb8b3fbcebaf093a4d8f2766c4a3a
Christian <c.espinoza.code@gmail.com>
2023-08-15 11:00:47 -0500

    feat: datGUI implemented

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ import { Orbit } from './components/Cameras/Orbit';
 import { Fullscreen } from './components/Fullscreen/Fullscreen';
 import { BufferGeometry } from './components/Geometries/BufferGeometry';
 import { BufferGeometryMultiples } from './components/Geometries/BufferGeometryMultiples';
+import { DatGUI } from './components/DebugUI/DatGUI';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <BufferGeometryMultiples />
+      <DatGUI />
     </div>
   );
 }
