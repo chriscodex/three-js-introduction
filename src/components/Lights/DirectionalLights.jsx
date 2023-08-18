8e584d2f12925ddb83d996be29398adb8e02cdb4
Christian <c.espinoza.code@gmail.com>
2023-08-18 14:05:00 -0500

    feat: directional light example added

---
+++ src/components/Lights/DirectionalLights.jsx
@@ -0,0 +1,127 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
+
+function DirectionalLights() {
+  const gui = new dat.GUI({ width: 400 });
+
+  /* Textures */
+  // Loading manager
+  const loadingManager = new THREE.LoadingManager();
+
+  // Canvas
+  const canvasRef = useRef(null);
+
+  // Scene - Like the scenario
+  const scene = new THREE.Scene();
+
+  /* Lights */
+  // Directional Light
+  const directionalLight = new THREE.DirectionalLight();
+  // directionalLight.color = new THREE.Color(0xffffff);
+  directionalLight.position.set(1, 0.5, 0);
+  directionalLight.intensity = 1;
+
+  scene.add(directionalLight);
+  // gui.add(directionalLight, 'intensity').min(0).max(1).setValue(0.01);
+
+  const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+  scene.add(directionalLightHelper);
+
+  // Sizes
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  // Material
+  const material = new THREE.MeshStandardMaterial();
+  material.roughness = 0.4;
+
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+  );
+  sphere.position.x = -1.5;
+
+  const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+  );
+
+  const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+  );
+  torus.position.x = 1.5;
+
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+  plane.rotation.x = -Math.PI * 0.5;
+  plane.position.y = -0.65;
+
+  scene.add(sphere, cube, torus, plane);
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.z = 3;
+  scene.add(camera);
+
+  useEffect(() => {
+    /* Resize */
+    window.addEventListener('resize', () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    // Controls
+    const controls = new OrbitControls(camera, canvasRef.current);
+    controls.enableDamping = true;
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    /* Animations */
+    const clock = new THREE.Clock();
+
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      // Control update for damping
+      controls.update();
+
+      // Rendering every tick
+      renderer.render(scene, camera);
+
+      // Method that call the next frame
+      window.requestAnimationFrame(tick);
+    };
+    tick();
+  }, []);
+
+  return (
+    <div>
+      <canvas className="webgl" ref={canvasRef}></canvas>
+    </div>
+  );
+}
+
+export { DirectionalLights };
