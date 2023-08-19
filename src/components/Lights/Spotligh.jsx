231cb44385b642b60ce6148a633a3e757a4661b8
Christian <c.espinoza.code@gmail.com>
2023-08-19 18:43:46 -0500

    feat: Spotlight example added

---
+++ src/components/Lights/Spotligh.jsx
@@ -0,0 +1,146 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
+import * as dat from 'dat.gui';
+
+function Spotligh() {
+  const gui = new dat.GUI({ width: 400 });
+
+  /* Textures */
+  // Loading manager
+  const loadingManager = new THREE.LoadingManager();
+
+  // Canvas
+  const canvasRef = useRef(null);
+
+  // Scene - Like the scenario
+  const scene = new THREE.Scene();
+
+  // Axis Helper
+  const axisHelper = new THREE.AxesHelper();
+  scene.add(axisHelper);
+
+  // Sizes
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  // Material
+  const material = new THREE.MeshStandardMaterial();
+  material.roughness = 0.4;
+
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+  );
+  sphere.position.x = -1.5;
+
+  const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+  );
+
+  const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+  );
+  torus.position.x = 1.5;
+
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+  plane.rotation.x = -Math.PI * 0.5;
+  plane.position.y = -0.65;
+
+  scene.add(sphere, cube, torus, plane);
+
+  /* Lights */
+  const spotLight = new THREE.SpotLight(
+    0xffffff,
+    0.5,
+    10,
+    Math.PI * 0.1,
+    0.25,
+    1
+  );
+  spotLight.position.set(0, 0, 5);
+  spotLight.intensity = 2;
+  // spotLight.castShadow = true;
+  // rectAreaLight.lookAt(cube.position)
+
+  scene.add(spotLight);
+  gui.add(spotLight, 'decay').min(0.1).max(100).setValue(0.01);
+
+  const spotLighHelper = new THREE.SpotLightHelper(spotLight);
+  scene.add(spotLighHelper);
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.z = 3;
+  scene.add(camera);
+
+  useEffect(() => {
+    /* Resize */
+    window.addEventListener('resize', () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    // Controls
+    const controls = new OrbitControls(camera, canvasRef.current);
+    controls.enableDamping = true;
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    /* Animations */
+    const clock = new THREE.Clock();
+
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      cube.rotation.y = elapsedTime * 0.1;
+      cube.rotation.x = elapsedTime * 0.15;
+      sphere.rotation.y = elapsedTime * 0.1;
+      sphere.rotation.x = elapsedTime * 0.15;
+      torus.rotation.y = elapsedTime * 0.1;
+      torus.rotation.x = elapsedTime * 0.15;
+
+      // Control update for damping
+      controls.update();
+
+      // Rendering every tick
+      renderer.render(scene, camera);
+
+      // Method that call the next frame
+      window.requestAnimationFrame(tick);
+    };
+    tick();
+  }, []);
+
+  return (
+    <div>
+      <canvas className="webgl" ref={canvasRef}></canvas>
+    </div>
+  );
+}
+
+export { Spotligh };
