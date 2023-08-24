5381675f92618f369ecabc39028ff9bba0211ac9
Christian <c.espinoza.code@gmail.com>
2023-08-23 21:54:25 -0500

    Model importation for helmet example initial structure created

---
+++ src/components/ModelsImportation/ModelImportationHelmet.jsx
@@ -0,0 +1,122 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+function ModelImportationHelmet() {
+  // Loading manager
+  const loadingManager = new THREE.LoadingManager();
+
+  // Canvas
+  const canvasRef = useRef(null);
+
+  // Scene - Like the scenario
+  const scene = new THREE.Scene();
+
+  // Models
+  const gltfLoader = new GLTFLoader(loadingManager);
+  gltfLoader.load('/21-models/Duck/glTF/Duck.gltf', (gltfModel) => {
+    scene.add(gltfModel.scene.children[0]);
+  });
+
+  /**
+   * Floor
+   */
+  const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+      color: '#444444',
+      metalness: 0,
+      roughness: 0.5,
+    })
+  );
+  floor.receiveShadow = true;
+  floor.rotation.x = -Math.PI * 0.5;
+  scene.add(floor);
+
+  /**
+   * Lights
+   */
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.set(1024, 1024);
+  directionalLight.shadow.camera.far = 15;
+  directionalLight.shadow.camera.left = -7;
+  directionalLight.shadow.camera.top = 7;
+  directionalLight.shadow.camera.right = 7;
+  directionalLight.shadow.camera.bottom = -7;
+  directionalLight.position.set(5, 5, 5);
+  scene.add(directionalLight);
+
+  // Sizes
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.set(2, 2, 2);
+  scene.add(camera);
+
+  useEffect(() => {
+    /* Resize */
+    window.addEventListener('resize', () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    // Controls
+    const controls = new OrbitControls(camera, canvasRef.current);
+    controls.enableDamping = true;
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    /* Animations */
+    const clock = new THREE.Clock();
+
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      // Control update for damping
+      controls.update();
+
+      // Rendering every tick
+      renderer.render(scene, camera);
+
+      // Method that call the next frame
+      window.requestAnimationFrame(tick);
+    };
+    tick();
+  }, []);
+
+  return (
+    <div>
+      <canvas className="webgl" ref={canvasRef}></canvas>
+    </div>
+  );
+}
+
+export { ModelImportationHelmet }
