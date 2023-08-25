cdc342ba8f043e7786a049413f1c2c109091a146
Christian <c.espinoza.code@gmail.com>
2023-08-24 20:05:55 -0500

    1 days ago
    feat: spinAngle defined

--- src/components/GalaxyGenerator/GalaxyGenerator.jsx
+++ src/components/GalaxyGenerator/GalaxyGenerator.jsx
@@ -23,7 +23,8 @@ function GalaxyGenerator() {
   parameters.size = 0.02;
   parameters.radius = 5;
   parameters.branches = 3;
-  parameters.spin = 13;
+  parameters.spin = 1;
+  parameters.randomness = 0.2;
 
   let geometry = null;
   let material = null;
@@ -45,12 +46,13 @@ function GalaxyGenerator() {
       const i3 = i * 3;
 
       const radius = Math.random() * parameters.radius;
+      const spinAngle = radius * parameters.spin;
       const branchAngle =
         ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-      positions[i3] = Math.cos(branchAngle) * radius;
+      positions[i3] = Math.cos(branchAngle + spinAngle) * radius;
       positions[i3 + 1] = 0;
-      positions[i3 + 2] = Math.sin(branchAngle) * radius;
+      positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
     }
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
@@ -98,6 +100,12 @@ function GalaxyGenerator() {
     .add(parameters, 'spin')
     .min(-5)
     .max(5)
+    .step(0.001)
+    .onFinishChange(generateGalaxy);
+  gui
+    .add(parameters, 'randomness')
+    .min(0)
+    .max(2)
     .step(0.001)
     .onFinishChange(generateGalaxy);
   /* Lights */
