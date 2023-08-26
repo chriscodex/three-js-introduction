1167aaa48cc294d8bc21bf5b6dbcd38ae2e01fb7
Christian <c.espinoza.code@gmail.com>
2023-08-25 20:19:40 -0500

    1 days ago
    feat: animation for objects added

--- src/components/Raycaster/Raycaster.jsx
+++ src/components/Raycaster/Raycaster.jsx
@@ -36,26 +36,38 @@ function Raycaster() {
   };
 
   /**
- * Objects
- */
-const object1 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-object1.position.x = - 2
+   * Objects
+   */
+  const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  );
+  object1.position.x = -2;
 
-const object2 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
+  const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  );
 
-const object3 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-object3.position.x = 2
+  const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  );
+  object3.position.x = 2;
 
-scene.add(object1, object2, object3)
+  scene.add(object1, object2, object3);
+
+  // Raycaster
+  const raycaster = new THREE.Raycaster();
+
+  // const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  // const rayDirection = new THREE.Vector3(10, 0, 0);
+  // rayDirection.normalize();
+
+  // raycaster.set(rayOrigin, rayDirection);
+
+  // const intersect = raycaster.intersectObject(object2);
+  // const intersects = raycaster.intersectObjects([object1, object2, object3]);
 
   // Camera
   const camera = new THREE.PerspectiveCamera(
@@ -99,6 +111,11 @@ scene.add(object1, object2, object3)
 
     const tick = () => {
       const elapsedTime = clock.getElapsedTime();
+
+      // Animate objets
+      object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+      object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+      object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
       // Control update for damping
       controls.update();
