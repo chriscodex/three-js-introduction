055f953f79fdf2b961487606b3e6bfbb76c64a2e
Christian <c.espinoza.code@gmail.com>
2023-08-25 20:21:04 -0500

    1 days ago
    feat: raycaster added in loop function

--- src/components/Raycaster/Raycaster.jsx
+++ src/components/Raycaster/Raycaster.jsx
@@ -113,9 +113,16 @@ function Raycaster() {
       const elapsedTime = clock.getElapsedTime();
 
       // Animate objets
-      object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
-      object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
-      object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+      object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+      object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+      object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+      // Cast a ray
+      const rayOrigin = new THREE.Vector3(-3, 0, 0);
+      const rayDirection = new THREE.Vector3(1, 0, 0);
+      rayDirection.normalize();
+
+      raycaster.set(rayOrigin, rayDirection);
 
       // Control update for damping
       controls.update();
